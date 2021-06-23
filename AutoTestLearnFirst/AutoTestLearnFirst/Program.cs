cf8595ac3f2228545346a2432a7f25539615df7a
Артём Бондаренко <36518701+BondarenkoCom@users.noreply.github.com>
2021-06-23 12:50:58 +0300

    Example Easy Test

---
+++ AutoTestLearnFirst/AutoTestLearnFirst/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace AutoTestLearnFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleParser.ParseAndSum("1");
+        }
+    }
+
+
+    class SimpleParserTests
+    {
+        public static void TestReturnsZeroWhenEmptyString()
+        {
+            try
+            {
+                SimpleParser p = new SimpleParser();
+                int result = p.ParseAndSum(string.Empty);
+                if (result !=0)
+                {
+                    Console.WriteLine(@”***SimpleParserTests.TestReturnsZeroWhenEmptyString:
+-------
+                }
+            }
+        }
+    }
+
+    public class SimpleParser
+    {
+        internal static void  ParseAndSum(string numbers)
+        {
+            if(numbers.Length==0)
+            {
+                Console.WriteLine(0);
+
+            }
+            if (!numbers.Contains(","))
+            {
+
+                Console.WriteLine(int.Parse(numbers));
+
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    "Пока умею обрабатывать только 0 или 1"
+                    );
+             } 
+        }
+    }
+}
